bc42336b82f5fb051c9cc335aca4701203c601c7
Hash52 <hirokinoakaunto@gmail.com>
2021-09-17 00:26:41 +0900

    動的に入力フォームを追加

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ const data = {
 };
 
 const options = {
-    title:{
+    title: {
         display: true,
         text: ''
     }
@@ -24,6 +24,12 @@ const config = {
 let graph;
 
 window.onload = function () {
+    const dataset_template = document.getElementById("dataset-template").content;
+    const fragment = document.createDocumentFragment();
+
+    fragment.appendChild(document.importNode(dataset_template, true));
+    document.getElementById('datasets').appendChild(fragment);
+
     document.getElementById('draw').addEventListener('click', function () {
         let graph_setting = document.forms['graph-setting'];
 
@@ -34,22 +40,27 @@ window.onload = function () {
 
         //FIXME
         //data.labels = graph_setting.elements['label[]'].values みたいな感じでループを回さずに取りたい
-        for(let i = 0; i < graph_setting.getElementsByClassName('dataset').length; i++){
+        for (let i = 0; i < graph_setting.getElementsByClassName('dataset').length; i++) {
             data.labels.push(graph_setting.elements['label[]'][i].value)
             data.datasets[0].backgroundColor.push(graph_setting.elements['color[]'][i].value)
             data.datasets[0].data.push(graph_setting.elements['data[]'][i].value)
         }
 
-        if(graph){
+        if (graph) {
             graph.config = config;
             graph.update();
-        }else{
+        } else {
             graph = new Chart(
                 document.getElementById('myChart'),
                 config
             );
         }
 
+    });
+
+    document.getElementById('add-dataset').addEventListener('click', function () {
+        fragment.appendChild(document.importNode(dataset_template, true));
+        document.getElementById('datasets').appendChild(fragment);
     })
 }
 

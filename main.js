810925aa2d024f8b348ce78ef66e6742cf12db86
Hash52 <hirokinoakaunto@gmail.com>
2021-09-16 23:06:55 +0900

    フォームの値をグラフに描画

--- main.js
+++ main.js
@@ -1,34 +1,54 @@
-const labels = [
-    'Red',
-    'Blue',
-    'Yellow'
-];
 
 const data = {
-    labels: labels,
+    labels: [],
     datasets: [{
-        label: 'My First Dataset',
-        data: [300, 50, 100],
-        backgroundColor: [
-            'rgb(255, 99, 132)',
-            'rgb(54, 162, 235)',
-            'rgb(255, 205, 86)'
-        ],
+        data: [],
+        backgroundColor: [],
         hoverOffset: 4
-    }]
+    }],
 };
+
+let options = {
+    title:{
+        display: true,
+        text: ''
+    }
+}
 
 const config = {
     type: 'pie',
     data: data,
+    options: options
 };
 
+let graph;
+
 window.onload = function () {
-    document.getElementById('draw').addEventListener('click',function(){
-        new Chart(
-            document.getElementById('myChart'),
-            config
-        );  
+    document.getElementById('draw').addEventListener('click', function () {
+        let graph_setting = document.forms['graph-setting'];
+
+        options.title.text = graph_setting.elements['title'].value;
+        data.labels = [];
+        data.datasets[0].data = [];
+        data.datasets[0].backgroundColor = [];
+
+        //FIXME
+        //data.labels = graph_setting.elements['label[]'].values みたいな感じでループを回さずに取りたい
+        for(let i = 0; i < graph_setting.getElementsByClassName('dataset').length; i++){
+            data.labels.push(graph_setting.elements['label[]'][i].value)
+            data.datasets[0].backgroundColor.push(graph_setting.elements['color[]'][i].value)
+            data.datasets[0].data.push(graph_setting.elements['data[]'][i].value)
+        }
+
+        if(graph){
+            graph.update(config);
+        }else{
+            graph = new Chart(
+                document.getElementById('myChart'),
+                config
+            );
+        }
+
     })
 }
 

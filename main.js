852ee039b7d707e8b00885e0eb97929f85d79e79
Hash52 <hirokinoakaunto@gmail.com>
2021-09-16 18:26:34 +0900

    円グラフサンプル描画 https://www.chartjs.org/docs/latest/charts/doughnut.html

--- main.js
+++ main.js
@@ -1,29 +1,29 @@
 const labels = [
-    'January',
-    'February',
-    'March',
-    'April',
-    'May',
-    'June',
+    'Red',
+    'Blue',
+    'Yellow'
 ];
 
 const data = {
     labels: labels,
     datasets: [{
-        label: 'My First dataset',
-        backgroundColor: 'rgb(255, 99, 132)',
-        borderColor: 'rgb(255, 99, 132)',
-        data: [0, 10, 5, 2, 20, 30, 45],
+        label: 'My First Dataset',
+        data: [300, 50, 100],
+        backgroundColor: [
+            'rgb(255, 99, 132)',
+            'rgb(54, 162, 235)',
+            'rgb(255, 205, 86)'
+        ],
+        hoverOffset: 4
     }]
 };
 
 const config = {
-    type: 'line',
+    type: 'doughnut',
     data: data,
-    options: {}
 };
 
-window.onload = function(){
+window.onload = function () {
     var myChart = new Chart(
         document.getElementById('myChart'),
         config
